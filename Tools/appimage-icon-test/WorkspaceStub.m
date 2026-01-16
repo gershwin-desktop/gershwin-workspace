84044fac1547e8ffd1921c3abb37834c9e1acfc7
probonopd <probonopd@users.noreply.github.com>
2026-01-16 08:27:22 +0100

    Fid build?

---
+++ Tools/appimage-icon-test/WorkspaceStub.m
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+
+// Minimal stub to satisfy linkage against symbols referenced by FSNode
+@interface Workspace : NSObject
+@end
+
+@implementation Workspace
+@end
