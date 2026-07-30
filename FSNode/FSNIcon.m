5319f1c33154806191b0ecf1fce2752d30dec1f4
probonopd <probonopd@users.noreply.github.com>
2026-07-30 23:45:51 +0200

    FSNIcon: scale highlight rect padding by GSScaleFactor
    hlightRect = iconSize + 6 was unscaled, so at 1.2x the 6px padding
    was proportionally smaller (6/77 = 7.8% vs 6/64 = 9.4% at 1.0).
    Apply ceil((iconSize + 6) * sf) to keep the highlight proportional
    to the scaled slot.

--- FSNode/FSNIcon.m
+++ FSNode/FSNIcon.m
@@ -245,9 +245,15 @@ static NSImage *branchImage;
       selectable = YES;
       isLeaf = YES;
 
-      hlightRect = NSZeroRect;
-      hlightRect.size.width = iconSize + 6;
-      hlightRect.size.height = hlightRect.size.width;
+      {
+        CGFloat sf = 1.0;
+        NSNumber *n = [[NSUserDefaults standardUserDefaults] objectForKey: @"GSScaleFactor"];
+        if (n) sf = [n floatValue];
+        if (sf < 1.0) sf = 1.0;
+        hlightRect = NSZeroRect;
+        hlightRect.size.width = ceil((iconSize + 6) * sf);
+        hlightRect.size.height = hlightRect.size.width;
+      }
       hlightRect = NSIntegralRect(hlightRect);
       ASSIGN (highlightPath, [fsnodeRep highlightPathOfSize: hlightRect.size]);
 
